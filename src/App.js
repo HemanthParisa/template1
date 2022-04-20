197fa0ff155a437737a0347131e8e8f72aa64b27
Hemanth Parisa <hemanthkumarparisa@gmail.com>
2022-04-20 16:39:56 +0530

    small template

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className='header'>
+        <div>company logo and slogan</div>
+       </div>
+      <div className='header1'>
+        <div>Header</div>
+      </div>
+     <div className='header2'>
+       <div>navigation....</div> 
+       <div></div>
+       <div></div>
+       <div></div>
+     </div>
+    <div className='header3'>
+      <div>about</div>
+      <div className='header33'>
+      <div>quicklines</div>
+      <div>quicklines</div>
+    </div>
+    </div>
+    <div className='header4'>
+      <div>site plan and copyrights info</div>
+    </div>
+    </div>
+
+    
+  );
+}
+
+export default App;
